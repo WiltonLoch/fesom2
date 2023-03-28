1d7987df74db7f784e040fd4b1a2e349d9214622
wiltonloch <wilton.loch@gmail.com>
2023-03-28 12:59:41 +0200

    chore: formatting solve_tridiag subroutine

--- src/cvmix_utils.F90
+++ src/cvmix_utils.F90
@@ -344,40 +344,43 @@ contains
 
 
 !=================================================================================
-  subroutine solve_tridiag(a,b,c,d,x,n)
-        implicit none
-  !---------------------------------------------------------------------------------
-  !        a - sub-diagonal (means it is the diagonal below the main diagonal)
-  !        b - the main diagonal
-  !        c - sup-diagonal (means it is the diagonal above the main diagonal)
-  !        d - right part
-  !        x - the answer
-  !        n - number of equations
-  !---------------------------------------------------------------------------------
-          integer,intent(in) :: n
-          real*8,dimension(n),intent(in) :: a,b,c,d
-          real*8,dimension(n),intent(out) :: x
-          real*8,dimension(n) :: cp,dp
-          real*8 :: m,fxa
-          integer i
+subroutine solve_tridiag(a,b,c,d,x,n)
+    implicit none
+    !---------------------------------------------------------------------------------
+    !        a - sub-diagonal (means it is the diagonal below the main diagonal)
+    !        b - the main diagonal
+    !        c - sup-diagonal (means it is the diagonal above the main diagonal)
+    !        d - right part
+    !        x - the answer
+    !        n - number of equations
+    !---------------------------------------------------------------------------------
+    integer, intent(in)                 :: n
+    real * 8, dimension(n), intent(in)  :: a, b, c, d
+    real * 8, dimension(n), intent(out) :: x
+    real * 8, dimension(n)              :: cp, dp
+    real * 8                            :: m, fxa
+    integer i
 
-  ! initialize c-prime and d-prime
-          cp(1) = c(1)/b(1)
-          dp(1) = d(1)/b(1)
-  ! solve for vectors c-prime and d-prime
-           do i = 2,n
-             m = b(i)-cp(i-1)*a(i)
-             fxa = 1D0/m
-             cp(i) = c(i)*fxa
-             dp(i) = (d(i)-dp(i-1)*a(i))*fxa
-           enddo
-  ! initialize x
-           x(n) = dp(n)
-  ! solve for x from the vectors c-prime and d-prime
-          do i = n-1, 1, -1
-            x(i) = dp(i)-cp(i)*x(i+1)
-          end do
-  end subroutine solve_tridiag
+    ! initialize c-prime and d-prime
+    cp(1) = c(1)/b(1)
+    dp(1) = d(1)/b(1)
+
+    ! solve for vectors c-prime and d-prime
+    do i = 2, n
+        m = b(i) - cp(i - 1) * a(i)
+        fxa = 1D0 / m
+        cp(i) = c(i) * fxa
+        dp(i) = (d(i) - dp(i - 1) * a(i)) * fxa
+    end do
+
+    ! initialize x
+    x(n) = dp(n)
+
+    ! solve for x from the vectors c-prime and d-prime
+    do i = n - 1, 1, -1
+        x(i) = dp(i) - cp(i) * x(i + 1)
+    end do
+end subroutine solve_tridiag
 
 !!!subroutine solve_tridiag(a,b,c,d,x,n)
 !!!
